3e5821c5f7c4c20422ede97b3a9b9a272f8232e0
Gabriela Zanetic <gabrielazanetic@Gabrielas-MacBook-Pro.local>
2022-08-29 18:22:31 +0200

    added navigation component to shared page template, refined logo

--- src/shared/Page.tsx
+++ src/shared/Page.tsx
@@ -1,18 +1,24 @@
 import { FC } from "react";
+import Navigation from "../components/Navigation";
 
 const Page: FC = () => {
     const logo = require("../logo.svg").default;
 
     return (
-        <>
-            {/* logo */}
-            <img src={logo} alt="Logo" className="logo" />
+        <header>
+            {/* logo and title */}
+            <div className="logo">
+                <img src={logo} alt="Logo" className="logo__image" />
+                <h3 className="logo__text">React Tutorial 2022</h3>
+            </div>
 
             {/* navigation */}
-
+                {/* menu */}
+                    {/* menu items */}
+            <Navigation />
 
             {/* login and registration / user menu */}
-        </>
+        </header>
     );
 }
 
