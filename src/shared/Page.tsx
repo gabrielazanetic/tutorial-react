53e6d8d8f430221f266278aeef16a781c2d8e795
Gabriela Zanetic <gabrielazanetic@Gabrielas-MacBook-Pro.local>
2022-08-29 17:57:21 +0200

    added logo

--- src/shared/Page.tsx
+++ src/shared/Page.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 
 const Page: FC = () => {
-    
+    const logo = require("../logo.svg").default;
+
     return (
         <>
             {/* logo */}
+            <img src={logo} alt="Logo" />
+
             {/* navigation */}
             {/* login and registration / user menu */}
         </>
