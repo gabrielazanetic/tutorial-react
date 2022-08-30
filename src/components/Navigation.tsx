1314edcbcc57e5c779268919ecba7e64c3eff172
Gabriela Zanetic <gabrielazanetic@Gabrielas-MacBook-Pro.local>
2022-08-30 09:30:22 +0200

    added additional link placeholders to main navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,18 +3,21 @@ import Menu from "./Menu";
 import MenuItem from "./MenuItem";
 
 const Navigation: FC = () => {
-
     return ( 
         <nav>
-            <Menu>
+            <Menu className="menu--navigation">
+                <MenuItem>Home</MenuItem>
                 <MenuItem>
-                    <h4>Home</h4>
-                </MenuItem>
-                <MenuItem>
-                    <Menu title="Submenu" collapsed={true}>
-                        <MenuItem>Submenu item</MenuItem>
+                    <Menu title="Tutorial" collapsed={true}>
+                        <MenuItem>Step 1</MenuItem>
+                        <MenuItem>Step 2</MenuItem>
+                        <MenuItem>Step 3</MenuItem>
+                        <MenuItem>Step 4</MenuItem>
+                        <MenuItem>Step 5</MenuItem>
                     </Menu>
                 </MenuItem>
+                <MenuItem>Useful links</MenuItem>
+                <MenuItem>Reviews</MenuItem>
             </Menu>
         </nav>
     );
