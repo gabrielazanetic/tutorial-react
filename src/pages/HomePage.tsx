fcec97ee35fa1a063b164bbdf4bff9805fa127d4
Gabriela Zanetic <gabriela.zanetic@deegloo.com>
2022-09-06 09:47:40 +0200

    added home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Page from "../shared/Page";
+
+const HomePage: FC = () => {
+    return (
+        <Page>
+            <p>Welcome to the React tutorial!</p>
+        </Page>
+    );
+};
+
+export default HomePage;
