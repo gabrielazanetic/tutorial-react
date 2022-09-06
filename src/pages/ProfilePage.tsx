813df86993dd00ff631e7eeedbd858695d39d775
Gabriela Zanetic <gabriela.zanetic@deegloo.com>
2022-09-06 10:18:43 +0200

    added initial profile page component

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Page from "../shared/Page";
+
+const ProfilePage: FC = () => {
+    return (
+        <Page>
+            <h1>Profile Page</h1>
+        </Page>
+    );
+};
+
+export default ProfilePage;
