6544659fa6c9fef3b86aa875cd5371c565d82233
Gabriela Zanetic <gabriela.zanetic@deegloo.com>
2022-09-06 10:18:31 +0200

    added 404 page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Page from "../shared/Page";
+
+const NotFoundPage: FC = () => {
+    return (
+        <Page>
+            <h1>404 PAGE</h1>
+        </Page>
+    );
+}
+
+export default NotFoundPage;
