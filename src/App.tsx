9a6d471f0f3c1e36e6dfca517f2be13730fbcc94
Gabriela Zanetic <gabriela.zanetic@deegloo.com>
2022-09-06 10:19:21 +0200

    added 404 page redirect route and profile page route auth check and redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import ProfilePage from './pages/ProfilePage';
 import TutorialPage from './pages/TutorialPage';
 
 function App() {
+const isLoggedIn = true;
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/tutorial" element={<TutorialPage />} />
+          <Route path="/my-profile" element={isLoggedIn ? <ProfilePage /> : <Navigate to="/anything" />} />
+
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </div>
